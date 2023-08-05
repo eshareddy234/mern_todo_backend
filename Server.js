42d696c4b2a7db5fc42a741bd273548a6bd57877
eshareddy234 <119891063+eshareddy234@users.noreply.github.com>
2023-08-05 23:20:21 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors(
-    {
-        origin: ["https://mern-todo-app-ten.vercel.app"],
-        methods: ["POST","GET","DELETE"],
-        credentials: true
-    }
+    // {
+    //     origin: ["https://mern-todo-app-ten.vercel.app"],
+    //     methods: ["POST","GET","DELETE"],
+    //     credentials: true
+    // }
 ));
 app.use(express.json());
 
